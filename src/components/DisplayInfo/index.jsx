7a28676226a8eff759dc3f8dc0ba0032acca9de5
Rafael Schug <rafaelschugbc@gmail.com>
2021-10-04 16:48:29 -0300

    add: DisplayInfo component shows all the required information

--- src/components/DisplayInfo/index.jsx
+++ src/components/DisplayInfo/index.jsx
@@ -1,4 +1,9 @@
 const DisplayInfo = ({transactions}) => {
+
+    const qtdTotalFrutasVendidas = transactions.reduce((acc, item)=> item.quantity < 0 ? acc + item.quantity * -1 : acc, 0);
+    const qtdTotalEntradaFrutas = transactions.reduce((acc, item)=> item.quantity > 0 ? acc + item.quantity : acc, 0);
+    const valorTotalSaidas = transactions.reduce((acc, item) => item.quantity < 0 ? acc + (item.price * (item.quantity * -1)) : acc, 0);
+
     return (
         <div>
             <h3>Todas as movimentações:</h3>
@@ -25,6 +30,11 @@ const DisplayInfo = ({transactions}) => {
                 </tbody>
             </table>    
         
+            <ul>
+                <li>Quantidade total de frutas vendidas: {qtdTotalFrutasVendidas}</li>
+                <li>Quantitade total de entrada de frutas: {qtdTotalEntradaFrutas}</li>
+                <li>Valor total de saída: $ {valorTotalSaidas}</li>
+            </ul>
         </div>
         
     )
--- src/components/FormControl/index.jsx
+++ src/components/FormControl/index.jsx
@@ -1,33 +1,70 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import './style.css';
 
 const FormControl = ({transactions, setTransactions}) => {
 
 
     const [type, setType] = useState('entrada');
     const [input, setInput] = useState({name:'', price:'', quantity:''});
+    const [error, setError] = useState(false);
 
     const handleSubmitForm = (event) => {
 
         event.preventDefault();
 
-        if(type === 'entrada'){
-            setTransactions([...transactions, input])
+        const checkInputFields = Object.values(input).every(item => item !== '' && item !== 0);
+
+        if(checkInputFields){
+            
+            if(type === 'entrada'){
+                setTransactions([...transactions, input])
+            } else {
+                setTransactions([...transactions, {...input, quantity: input.quantity * -1}])
+            }
+            
+            setInput({name:'', price:'', quantity:''})
+
         } else {
-            setTransactions([...transactions, {...input, quantity: input.quantity * -1}])
+            setError(true);
         }
     }
 
+    useEffect(() => {
+        const checkInputFields = Object.values(input).every(item => item !== '' && item !== 0);
+
+        if(error && checkInputFields){
+            setError(false);
+        }
+        
+    }, [input, error])
+
     return (
-        <div>
+        <div className='form__container'>
+            <h3>Controle de entrada/saída</h3>
             <form onSubmit={handleSubmitForm}>
-                <select onChange={(e) => setType(e.target.value)}>
-                    <option defaultValue value="entrada">Entrada</option>
-                    <option value="saida">Saída</option>
-                </select>
-                <input type="text" placeholder='Nome' onChange={e => setInput({...input, name: e.target.value})} value={input.name}/>
-                <input type="number" placeholder='Quantidade' onChange={e => setInput({...input, quantity: Number(e.target.value)})} value={input.quantity}/>
-                <input type="input" placeholder='Valor' onChange={e => setInput({...input, price: Number(e.target.value)})} value={input.price}/>
-                <button type='submit'>Adicionar</button>
+                {error && <span>Preencha todos os campos<br/>Quantidade/Valor precisam ser maiores que 0</span>}
+                <div>
+                    <label htmlFor="">Tipo:</label>
+                    <select onChange={(e) => setType(e.target.value)}>
+                        <option defaultValue value="entrada">Entrada</option>
+                        <option value="saida">Saída</option>
+                    </select>
+                </div>
+                <div>
+                    <label htmlFor="">Nome</label>
+                    <input type="text" placeholder='Nome' onChange={e => setInput({...input, name: e.target.value})} value={input.name}/>
+                </div>
+               <div>
+                   <label htmlFor="">Quantidade</label>
+                   <input type="number" placeholder='Quantidade' onChange={e => setInput({...input, quantity: Number(e.target.value)})} value={input.quantity}/>
+               </div>
+                <div>
+                    <label htmlFor="">Valor</label>
+                    <input type="input" placeholder='Valor' onChange={e => setInput({...input, price: Number(e.target.value)})} value={input.price}/>
+                </div>
+                <div>
+                    <button type='submit'>Adicionar</button>
+                </div>
             </form>
         </div>
     )
